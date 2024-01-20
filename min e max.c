e14a1c384b8db3e876814c79758d51922f1ecdd1
mcclara18 <141859035+mcclara18@users.noreply.github.com>
2024-01-19 23:20:39 -0300

    Add files via upload

---
+++ min e max.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+// Função para calcular o valor mínimo
+float cminimo (float num[], int tamanho) {
+    float min = num[0];
+
+    for (int i = 1; i < tamanho; ++i) {
+        if (num[i] < min) {
+            min = num[i];
+        }
+    }
+
+    return min;
+}
+
+// Função para calcular o valor máximo
+float cmax(float num[], int tamanho) {
+    float max = num[0];
+
+    for (int i = 1; i < tamanho; ++i) {
+        if (num[i] > max) {
+            max = num[i];
+        }
+    }
+
+    return max;
+}
+
+int main() {
+    float num[100];  // Suponhamos que não teremos mais de 100 números
+    int contador = 0;
+    float numero;
+
+    printf("Digite uma sequência (insira um número negativo para encerrar):\n");
+
+    while (1) {
+        scanf("%f", &numero);
+
+        if (numero < 0) {
+            break;
+        }
+
+        num[contador] = numero;
+        ++contador;
+    }
+
+    if (contador == 0) {
+        printf("Insira um número.\n");
+    } else {
+        printf("O valor mínimo é: %.2f\n", cminimo(num, contador));
+        printf("O valor máximo é: %.2f\n", cmax(num, contador));
+    }
+
+    return 0;
+}
